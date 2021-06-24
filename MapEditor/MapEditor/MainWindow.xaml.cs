39e1a538966f5af173b16aebfa3c0435c055ef04
Vasa <bershak.vasya@mail.ru>
2021-06-24 14:49:06 +0300

    adding

--- MapEditor/MapEditor/MainWindow.xaml.cs
+++ MapEditor/MapEditor/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace MapEditor
         Image image = new Image();
         BitmapImage bitmapImage = new BitmapImage();
         private string filename;
+        private List<Color> col = new List<Color>();
         public MainWindow()
         {
             InitializeComponent();
@@ -38,16 +39,71 @@ namespace MapEditor
             OpenImage();
             ReadImage();
         }
+        private bool IsExist(Color c)
+        {
+            for (int i = 0; i < col.Count; i++)
+            {
+                if (c == col[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void BroseClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            Image im = new Image();
+            dialog.Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+            if (dialog.ShowDialog() == true)
+            {
+                BitmapImage bit = new BitmapImage();
+                bit.BeginInit();
+                bit.UriSource = new Uri(dialog.FileName);
+                bit.EndInit();
+                im.Source = bit;
+            }
+
+
+        }
         private void ReadImage()
         {
             System.Drawing.Bitmap b = new System.Drawing.Bitmap(filename);
             //WriteLine(b.Palette.Entries.Length);
             //b.Palette.Entries.ToList().ForEach(x =>  System.Diagnostics.Debug.WriteLine($"R: {x.R}, G: {x.G}, B: {x.B}, A: {x.A}"));
-            var color = b.GetPixel(50, 50);
-            Main.Background = new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
             int imageWidth = bitmapImage.PixelWidth;
             int imageHeight = bitmapImage.PixelHeight;
-            
+            int schet = 0;
+            for (int i = 0; i < imageWidth; i+=4)
+            {
+                for (int j = 0; j < imageHeight; j+=4)
+                {
+                    var color = b.GetPixel(i, j);
+                    Color c = (new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B))).Color;
+                    if (!IsExist(c))
+                    {
+                        
+                        ColorsChoice.RowDefinitions.Add(new RowDefinition());
+                        col.Add(c);
+                        Rectangle r = new Rectangle();
+                        r.Margin = new Thickness(30);
+                        r.Fill = new SolidColorBrush(c);
+                        ColorsChoice.Children.Add(r);
+                        r.ToolTip = c.ToString();
+                        Grid.SetRow(r, schet);
+                        Grid.SetColumn(r, 0);
+                       
+                        Button but = new Button();
+                        but.Content = "Browse...";
+                        but.Margin = new Thickness(30, 10, 30, 10);
+                        Grid.SetRow(but, schet);
+                        Grid.SetColumn(but, 2);
+                        ColorsChoice.Children.Add(but);
+                        but.Click += BroseClick;
+                        schet++;
+                    }
+                }
+            }
             int z = 0;
         }
         private void OpenImage()
@@ -66,5 +122,10 @@ namespace MapEditor
             column1.Children.Add(image);
             OpenMap.Visibility = Visibility.Hidden;
         }
+    }
+
+    public static class Addition
+    {
+        public static void Print(this string text) => WriteLine(text);
     }
 }
